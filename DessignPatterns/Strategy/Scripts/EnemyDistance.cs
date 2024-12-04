7130066da3b0b6cdb860ebbcda604490a840b268
Elio Alejandro Marcano Brito <alejomarcano.20@gmail.com>
2024-12-04 16:29:12 -0400

    Improve skills management

--- DessignPatterns/Strategy/Scripts/EnemyDistance.cs
+++ DessignPatterns/Strategy/Scripts/EnemyDistance.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using EnemyBullet;
+using System.Collections.Generic;
 
 public class EnemyDistance : Enemy
 {
-    protected Coroutine CoroutineAbility;
-    [SerializeField] protected EnemyAbility<GameObject> Ability;
+    protected List<Coroutine> CoroutineAbility;
+    [SerializeField] protected List<EnemyAbility<GameObject>> Abilities 
+                                        = new List<EnemyAbility<GameObject>>();
     void OnEnable()
     {
         InitAbilities();
@@ -17,17 +19,33 @@ public class EnemyDistance : Enemy
     void InitAbilities(){
         MyStart();
         if(_enemyFollow && CoroutineAbility == null){
+            CoroutineAbility = new List<Coroutine>(Abilities.Count);
             ExecuteAbilities();
         }
     }
+    /// <summary>
+    /// Stop all abilities of enemy (coroutine list)
+    /// </summary>
     void StopAbilities(){
         if(_enemyFollow && CoroutineAbility != null){
-            StopAbilites(CoroutineAbility);
+            foreach (var item in CoroutineAbility)
+            {
+                StopAbilites(item);   
+            }
         }
     }
+    /// <summary>
+    /// Init all abilities of enemy
+    /// </summary>
     protected virtual void ExecuteAbilities(){
-        CoroutineAbility = StartCoroutine(
-        Ability.ExecuteAbility(_enemyData,_player,gameObject));
+        CoroutineAbility.Clear();
+        foreach (var item in Abilities)
+        {
+            if(item != null){
+                Coroutine coroutine = StartCoroutine(item.ExecuteAbility(_enemyData, _player, gameObject));
+                CoroutineAbility.Add(coroutine);
+            }
+        }
     }
     protected virtual void StopAbilites(Coroutine coroutine){
         StopCoroutine(coroutine);
